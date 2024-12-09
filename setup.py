a88719ebacec0f57c5f68ee248f4a6a32f6fc54b
AlexAlbers2001 <alexander.albers@gmx.net>
2024-12-09 17:35:28 +0100

    More Info Feature, Diagram marker, Game History, info for config and setup file, some code cleanup

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ import sys
 import os
 from cx_Freeze import setup, Executable
 
-# python setup.py build
+# File for creating standalone executable for oekolopoly 2.0
+# invoke: python setup.py build
 
 files_to_include = ['oekolopoly/my_oekolopoly_gui/assets', 'oekolopoly/my_oekolopoly_gui/trained_agents',
                     'oekolopoly/my_oekolopoly_gui/bin', 'oekolopoly/my_oekolopoly_gui/Spielanleitung.pdf',
-                    'oekolopoly/my_oekolopoly_gui/feedback.txt']
+                    'oekolopoly/my_oekolopoly_gui/feedback.txt','oekolopoly/my_oekolopoly_gui/current_game_history.txt']
 
 packages_to_include = ['oekolopoly']
 
@@ -19,7 +20,7 @@ executable = Executable(
 setup(
     name="Oekolopoly_v1",
     version="1.0",
-    description="Erster test lol",
+    description="Oekolopoly",
     author="Me",
     options={'build_exe': {'include_files': files_to_include, 'packages': packages_to_include}},
     executables=[executable]
